a845483ac7963971734f1b4fff0ec4fa028f2b27
assasinos <kubasem198@gmail.com>
2023-09-03 20:27:51 +0200

    Add properties and constructor

--- Overlay.cs
+++ Overlay.cs
@@ -1,11 +1,21 @@
 ﻿using System.Diagnostics;
+using SkiaSharp;
 
 namespace OverlayLibrary;
 
 public class Overlay : IDisposable
 {
+    private Process _overlayedProcess;
+    private GRContext _grContext;
+    private SKSurface _skSurface;
+    public SKCanvas _skCanvas;
 
-    
+
+    public Overlay(Process overlayedProcess)
+    {
+        _overlayedProcess = overlayedProcess;
+    }
+
     public void Dispose()
     {
         throw new NotImplementedException();
