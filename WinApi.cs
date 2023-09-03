f53b8f1ee5f1737aa6f9552cb23608602b709537
assasinos <kubasem198@gmail.com>
2023-09-03 20:34:26 +0200

    Add Windows functions

--- WinApi.cs
+++ WinApi.cs
@@ -1,6 +1,28 @@
-﻿namespace OverlayLibrary;
+﻿using System.Runtime.InteropServices;
+
+namespace OverlayLibrary;
 
 public class WinApi
 {
+    [DllImport("user32.dll", SetLastError = true)]
+    public static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+    [DllImport("user32.dll")]
+    public static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+    public const int GWL_EXSTYLE = -20;
+    public const int WS_EX_LAYERED = 0x80000;
+    public const int WS_EX_TRANSPARENT = 0x20;
     
+    
+    
+    [DllImport("user32.dll", SetLastError=true)]
+    static extern bool GetWindowRect(IntPtr hwnd, out RECT lpRect);
+    
+    [StructLayout(LayoutKind.Sequential)]
+    public struct RECT
+    {
+        public int Left;        // x position of upper-left corner
+        public int Top;         // y position of upper-left corner
+        public int Right;       // x position of lower-right corner
+        public int Bottom;      // y position of lower-right corner
+    }
 }
