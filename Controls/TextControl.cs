ff31fb81f58a9ac7b38b3601a662ddf0092689d3
assasinos <kubasem198@gmail.com>
2023-09-09 18:38:24 +0200

    Add TextControl

---
+++ Controls/TextControl.cs
@@ -0,0 +1,43 @@
+﻿using SkiaSharp;
+
+namespace OverlayLibrary;
+
+public class TextControl : IControl
+{
+    
+    private string Text { get; set; }
+
+    private SKPaint Paint { get; set; }
+    
+    public TextControl(string text, SKPaint paint)
+    {
+        Text = text;
+        Paint = paint;
+    }
+
+    void UpdateText(string text)
+    {
+        Text = text;
+    }
+    
+    void UpdatePaint(SKPaint paint)
+    {
+        Paint = paint;
+    }
+    
+
+    
+    public WinApi.RECT CalculateControlRect()
+    {
+
+        var bound = new SKRect();
+        Paint.MeasureText(Text, ref bound);
+        return new WinApi.RECT()
+        {
+            Left = (int)bound.Left,
+            Top = (int)bound.Top,
+            Right = (int)bound.Right,
+            Bottom = (int)bound.Bottom
+        };
+    }
+}
