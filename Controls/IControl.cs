82d5684f908a04fcc30fc9e320e7fa5182348896
assasinos <kubasem198@gmail.com>
2023-09-09 18:38:01 +0200

    Add Control interface

---
+++ Controls/IControl.cs
@@ -0,0 +1,14 @@
+﻿using OverlayLibrary;
+
+using static OverlayLibrary.WinApi;
+
+namespace OverlayLibrary;
+
+public interface IControl
+{
+
+
+    
+    
+    public RECT CalculateControlRect();
+}
