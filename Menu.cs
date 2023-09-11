456cbcbfa4d55ed16d3263d48eab5ba3500ca3ce
assasinos <kubasem198@gmail.com>
2023-09-11 15:50:25 +0200

    Add a way of displaying menus

--- Menu.cs
+++ Menu.cs
@@ -1,41 +1,100 @@
-﻿using SkiaSharp;
+﻿using System.Numerics;
+using OverlayLibrary.Controls;
+using SkiaSharp;
 using static OverlayLibrary.WinApi;
 
 namespace OverlayLibrary;
 
 public class Menu
 {
+    private const int MenuPadding = 10;
+    private const int ControlBottomMargin = 10;
+    
+    
     
     public string Name { get; set; }
     
     public RECT MenuRect { get; set; }
 
-    private readonly List<IControl> _menuControls;
+    private readonly List<IControl> _menuControls = new();
     
     private SKPoint _position;
 
-    public Menu(string name, List<IControl> menuControls, SKPoint position)
+    public Menu(string name, SKPoint position)
     {
         Name = name;
-        _menuControls = menuControls;
         _position = position;
     }
 
 
-    public RECT CalculateAllControlsRect()
+    public void AddControl(IControl control)
     {
-        var rect = new RECT();
+        _menuControls.Add(control);
+    }
+    
+    public void RemoveControl(IControl control)
+    {
+        _menuControls.Remove(control);
+    }
+    
+    /// <summary>
+    /// Calculates the size of the controls
+    /// </summary>
+    /// <returns>
+    /// Minimal width and height of the menu to fit all controls
+    /// </returns>
+    private Vector2 CalculateAllControlsRect()
+    {
+        var vec = new Vector2();
+
         
+        
+        foreach (var controlRect in _menuControls.Select(control => control.CalculateControlRect()))
+        {
+            //add some margin on bottom
+            vec.Y += controlRect.Y + ControlBottomMargin;
+            vec.X = Math.Max(vec.X, controlRect.X);
+        }
+        
+        
+        return vec;
+    }
+
+    public void Draw(SKCanvas skCanvas)
+    {
+        var allControlsRect = CalculateAllControlsRect();
+        //Draw Menu background
+        
+        var menuRect = new SKRect(_position.X - MenuPadding, _position.Y - MenuPadding, _position.X + allControlsRect.X + MenuPadding * 2, _position.Y + allControlsRect.Y + MenuPadding * 2);
+        
+        skCanvas.DrawRect(
+        menuRect,
+            new SKPaint()
+        {
+            Color = new SKColor(0, 0, 0, 150),
+            Style = SKPaintStyle.Fill
+        });
+        
+        
+        //Draw Menu Border
+        skCanvas.DrawRect(
+            menuRect,
+            new SKPaint()
+            {
+                Color = new SKColor(0, 0, 0, 255),
+                Style = SKPaintStyle.Stroke
+            });
+        
+        //Draw Menu Header with name
+        
+        
+        //Draw Each control
+        var currentY = _position.Y;
         foreach (var control in _menuControls)
         {
-            var controlRect = control.CalculateControlRect();
-            rect.Left = Math.Min(rect.Left, controlRect.Left);
-            rect.Top = Math.Min(rect.Top, controlRect.Top);
-            rect.Right = Math.Max(rect.Right, controlRect.Right);
-            //Add 10 pixels to bottom to make it look better
-            rect.Bottom = Math.Max(rect.Bottom, controlRect.Bottom + 10);
+            control.DrawControl(new SKPoint(_position.X, currentY), skCanvas);
+            
+            currentY += control.CalculateControlRect().Y + ControlBottomMargin;
         }
-
-        return rect;
     }
 }
