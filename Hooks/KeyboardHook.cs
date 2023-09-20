0fd547de93c297ec3855120924eec1aff676dc03
assasinos <kubasem198@gmail.com>
2023-09-20 11:03:25 +0200

    Add keyboard hook class and implement it

---
+++ Hooks/KeyboardHook.cs
@@ -0,0 +1,36 @@
+﻿namespace OverlayLibrary.Hooks;
+using static WinApi;
+
+public class KeyboardHook
+{
+    private VK _registeredKey;
+
+    public event EventHandler? KeyPressed;
+    
+    public KeyboardHook(VK registeredKey)
+    {
+        _registeredKey = registeredKey;
+        var thread = new Thread(Start);
+        thread.Start();
+    }
+
+    private void Start()
+    {
+        while (true)
+        {
+            
+            if ((GetAsyncKeyState(_registeredKey) & 0x01) == 1 )
+            {
+                OnKeyPressed();
+            }
+            
+            Thread.Sleep(10);
+        }
+        
+    }
+
+    protected virtual void OnKeyPressed()
+    {
+        KeyPressed?.Invoke(this, EventArgs.Empty);
+    }
+}
